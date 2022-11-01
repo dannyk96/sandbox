9c18ca9b01117383a22ad53dc19041e6acfdc19c
Daniel Kidger <daniel.kidger@gmail.com>
2022-11-01 15:58:33 +0000

    FC doesn't work on Windows, changed to F90

--- snow/build.sh
+++ snow/build.sh
@@ -6,17 +6,18 @@
 #  daniel.kidger@gmail.com
 #
 rm *.o *.mod
-FC=$(FC)
-$(FC) -c -g segments.f90
-$(FC) -cpp -DPGPLOT -c -g plot.f90
-$(FC) -c -g snow.f90
+#FCC=$(FC)
+F90=gfortran
+$F90 -c -g segments.f90
+$F90 -cpp -DPGPLOT -c -g plot.f90
+$F90 -c -g snow.f90
 echo 'note that we do not use afront.f90 (in a future release)'
-$(FC) -o snow snow.o segments.o plot.o -L$HOME/pgplot -lpgplot -lX11
+$F90 -o snow snow.o segments.o plot.o -L$HOME/Documents/danfe/pgplot -lpgplot -lX11
 
 echo try this:
 echo 'echo -e "/XWIN\n3\n2\n.4\n3\n\n\n" | ./snow'
 
-# notes on Salfrod FTN95
+# notes on Salford FTN95
 # ftn95 segments.f90 & ftn95 /CFPP /DEFINE SALFORD  1 plot.f90 & ftn95 snow.f90 /LINK  
 
 
